bae6144b8b4766fa0e293eec8a3d00c25b268f61
lovers2020 <dkrkskswl@naver.com>
2024-06-05 12:54:30 +0900

    update

--- app.py
+++ app.py
@@ -87,23 +87,23 @@ with app.app_context():
     db.create_all()
 
 # 최초 1회만 실행
-connection = psycopg2.connect(
-    host="dpg-cpfmi9n109ks73bne8rg-a",
-    dbname="apartment_db_kx9l",
-    user="apartment_db_kx9l_user",
-    password="OE3vQp19JOsVSQsUHz5TNRvbQbgJaIvT",
-    port="5432",
-)
+# connection = psycopg2.connect(
+#     host="dpg-cpfmi9n109ks73bne8rg-a",
+#     dbname="apartment_db_kx9l",
+#     user="apartment_db_kx9l_user",
+#     password="OE3vQp19JOsVSQsUHz5TNRvbQbgJaIvT",
+#     port="5432",
+# )
 
-cur = connection.cursor()
+# cur = connection.cursor()
 
-file = open("./resource/주소데이터_업데이트쿼리.sql", encoding="utf-8")
-for i in file:
-    cur.execute(i)
+# file = open("./resource/주소데이터_업데이트쿼리.sql", encoding="utf-8")
+# for i in file:
+#     cur.execute(i)
 
 
-connection.commit()
-connection.close()
+# connection.commit()
+# connection.close()
 
 
 # Test로 입력한 데이터 제거
@@ -335,4 +335,4 @@ def update_trade_info():
 
 
 if __name__ == "__main__":
-    app.run()
+    
