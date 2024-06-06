ceac812818544ae09524a4289ac325d02ba5e97c
lovers2020 <dkrkskswl@naver.com>
2024-06-07 02:11:47 +0900

    update

--- resource/주소데이터_업데이트쿼리.sql
+++ resource/주소데이터_업데이트쿼리.sql
@@ -2759,6 +2759,18 @@ INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125626', 
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125627', '경기도 용인시 처인구 이동읍 묵리', '41461');
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125628', '경기도 용인시 처인구 이동읍 천리', '41461');
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125629', '경기도 용인시 처인구 이동읍 서리', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125900', '경기도 용인시 처인구 남사읍', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125921', '경기도 용인시 처인구 남사읍 봉무리', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125922', '경기도 용인시 처인구 남사읍 북리', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125923', '경기도 용인시 처인구 남사읍 통삼리', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125924', '경기도 용인시 처인구 남사읍 봉명리', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125925', '경기도 용인시 처인구 남사읍 진목리', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125926', '경기도 용인시 처인구 남사읍 원암리', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125927', '경기도 용인시 처인구 남사읍 전궁리', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125928', '경기도 용인시 처인구 남사읍 방아리', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125929', '경기도 용인시 처인구 남사읍 창리', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125930', '경기도 용인시 처인구 남사읍 아곡리', '41461');
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146125931', '경기도 용인시 처인구 남사읍 완장리', '41461');
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146132000', '경기도 용인시 처인구 남사면', '41461');
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146132021', '경기도 용인시 처인구 남사면 봉무리', '41461');
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('4146132022', '경기도 용인시 처인구 남사면 북리', '41461');
