2aff2193a18eb975bac3792a3670cbe8efdfb99e
lovers2020 <dkrkskswl@naver.com>
2024-06-07 12:01:46 +0900

    update

--- resource/주소데이터_추가쿼리.sql
+++ resource/주소데이터_추가쿼리.sql
@@ -1,11 +1,8 @@
-INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313400', '경상남도 창원시 성산구 용지동', '48123');
-INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313500', '경상남도 창원시 성산구 용호동', '48123');
-INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313600', '경상남도 창원시 성산구 신월동', '48123');
-INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313700', '경상남도 창원시 성산구 대원동', '48123');
-INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313800', '경상남도 창원시 성산구 두대동', '48123');
-INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313900', '경상남도 창원시 성산구 삼동동', '48123');
-INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812314000', '경상남도 창원시 성산구 덕정동', '48123');
-INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812314100', '경상남도 창원시 성산구 퇴촌동', '48123');
-
-
-
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313400', '경상남도 창원시 성산구 용지동', '48123')
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313500', '경상남도 창원시 성산구 용호동', '48123')
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313600', '경상남도 창원시 성산구 신월동', '48123')
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313700', '경상남도 창원시 성산구 대원동', '48123')
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313800', '경상남도 창원시 성산구 두대동', '48123')
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812313900', '경상남도 창원시 성산구 삼동동', '48123')
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812314000', '경상남도 창원시 성산구 덕정동', '48123')
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('4812314100', '경상남도 창원시 성산구 퇴촌동', '48123')
