db6e1a1723546c5e01f2478686ff0d9a6d39d8e2
lovers2020 <dkrkskswl@naver.com>
2024-06-07 12:40:47 +0900

    update

--- resource/주소데이터_추가쿼리.sql
+++ resource/주소데이터_추가쿼리.sql
@@ -7,7 +7,7 @@ INSERT INTO address_codes(code, address_name, parent_code) VALUES('2671025925', 
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('2671025926', '부산광역시 기장군 일광읍 신평리', '26710');
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('2671025927', '부산광역시 기장군 일광읍 칠암리', '26710');
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('2671025928', '부산광역시 기장군 일광읍 문동리', '26710');
-INSERT INTO address_codes(code, address_name, parent_code) VALUES('2671025929', '부산광역시 기장군 일광읍 청광리', '26710';)
+INSERT INTO address_codes(code, address_name, parent_code) VALUES('2671025929', '부산광역시 기장군 일광읍 청광리', '26710');
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('2671025930', '부산광역시 기장군 일광읍 원리', '26710');
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('2671025931', '부산광역시 기장군 일광읍 화전리', '26710');
 INSERT INTO address_codes(code, address_name, parent_code) VALUES('2671025932', '부산광역시 기장군 일광읍 횡계리', '26710');
